fac16b2b62bc6dae72c13b2bfe5ddcfbbf4d7bf3
Aira Sakajiri <a.sakajiri@appguard.us>
2018-09-26 15:32:59 +0900

    例外処理の追加

--- write_registry.py
+++ write_registry.py
@@ -5,22 +5,25 @@ print('control panel\desktop の ScreenSaveActive値 を変更します')
 
 path = r'control panel\desktop'
 
-key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, path, access=winreg.KEY_READ)
-data, regtype = winreg.QueryValueEx(key, 'ScreenSaveActive')
-winreg.CloseKey(key)
+try:
+	key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, path, access=winreg.KEY_READ)
+	data, regtype = winreg.QueryValueEx(key, 'ScreenSaveActive')
+	winreg.CloseKey(key)
 
-print('data = ' + data)
+	print('data = ' + data)
 
-if data == '1':
-	data = '0'
-else:
-	data = '1'
+	if data == '1':
+		data = '0'
+	else:
+		data = '1'
 
-print('値を ' + data + ' に変更します')
+	print('値を ' + data + ' に変更します')
 
-key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, path, access=winreg.KEY_WRITE)
-winreg.SetValueEx(key, 'ScreenSaveActive', 0, winreg.REG_SZ, data)
-winreg.CloseKey(key)
+	key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, path, access=winreg.KEY_WRITE)
+	winreg.SetValueEx(key, 'ScreenSaveActive', 0, winreg.REG_SZ, data)
+	winreg.CloseKey(key)
+except:
+	print("読込、または書込時にエラーが発生しました")
 
 print("-- 終了 --")
 input('Press Enter...')
